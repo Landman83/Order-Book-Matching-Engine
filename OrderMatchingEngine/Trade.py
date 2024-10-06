b52c5992bf63043e01b1f10ed6fd71cbb9ca849e
Landman83 <rpoage@proton.me>
2024-10-06 08:43:01 -0500

    Update Trade.py to accommodate trader identification

--- OrderMatchingEngine/Trade.py
+++ OrderMatchingEngine/Trade.py
@@ -1,5 +1,6 @@
 from OrderMatchingEngine.Order import *
 from time import time
+import secrets
 
 class Trade(object):
 	"""
@@ -8,13 +9,16 @@ class Trade(object):
 
 	A trade object
 	"""
-	def __init__(self, incoming_side: Side, price: float, trade_size: int, incoming_order_id: int, book_order_id: int):
-		self.timestamp = int(1e6 * time())
-		self.side = incoming_side
-		self.price = price
-		self.size = trade_size
+	def __init__(self, incoming_order_id, book_order_id, incoming_side, price, size, buyer_id, seller_id):
 		self.incoming_order_id = incoming_order_id
 		self.book_order_id = book_order_id
+		self.incoming_side = incoming_side
+		self.price = price
+		self.size = size
+		self.buyer_id = buyer_id
+		self.seller_id = seller_id
 
 	def __repr__(self):
-		return 'Executed: {0} {1} units at {2}'.format(self.side, self.size, self.price)
+		return (f"Trade: Incoming Order ID: {self.incoming_order_id}, Book Order ID: {self.book_order_id}, "
+				f"Incoming Side: {self.incoming_side}, Price: {self.price}, Size: {self.size}, "
+				f"Buyer ID: {self.buyer_id}, Seller ID: {self.seller_id}")
